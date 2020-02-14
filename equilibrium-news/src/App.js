5effbd704e41e8a838363fb38c5fe08f7bd52972
anacanm <anacan@mangelsdorf.io>
2020-02-13 17:03:43 -0800

    removed RecommendedBubble component and made a RecommendedBubbleContainer :)

--- equilibrium-news/src/App.js
+++ equilibrium-news/src/App.js
@@ -2,20 +2,26 @@ import React, { useState } from 'react';
 import { QueriedArticlesContainer } from './containers/QueriedArticlesContainer';
 import { SearchBar } from './components/SearchBar';
 import { TopArticlesContainer } from './containers/TopArticlesContainer';
-import { RecommendedBubble } from './components/RecommendedBubble'
+import { RecommendedBubbleContainer } from './containers/RecommendedBubbleContainer';
 
 
 export const App = () => {
     
     const [query, setQuery] = useState("");  
-    //query is at an initial state of an empty string, rather than setting a default query here, I'm handling defaults in the service to avoid difficulties
-    //there likely won't be any default user-queried display in the final product(since we will be using the home page), but just to let you know
+
+    /**
+    * query is at an initial state of an empty string, rather than setting a default query here, I'm handling defaults in the service to avoid difficulties
+    * there likely won't be any default user-queried display in the final product(since we will be using the home page), but just to let you know -anacan
+    */
     
+    const keywords = ["politics", "feminism", "technology", "business", "health"];
+
+
+
     return(
         <div>
             <SearchBar setQuery={(setQuery)}/>
-            <RecommendedBubble setQuery = {(setQuery)} keyword = "politics"/>
-            <RecommendedBubble setQuery = {(setQuery)} keyword = "feminism"/>
+            <RecommendedBubbleContainer setQuery = {(setQuery)} keywords = {keywords}/>
             <QueriedArticlesContainer query = {query}/>
             <TopArticlesContainer/>
         </div>
