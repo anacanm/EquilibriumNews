5e3cee7422301e831ef07810338f9506de803dd6
anacanm <anacan@mangelsdorf.io>
2020-02-14 16:47:43 -0800

    handling null author value

--- equilibrium-news/src/components/Article.js
+++ equilibrium-news/src/components/Article.js
@@ -21,15 +21,21 @@ export const Article = (props) =>{
         if(showArticle){setFrameStyle({display:"flex", border: "none"})}
         else{setFrameStyle({display: "none", border: "none"})}
     }, [showArticle])
+
+    let authorText = ""
+    if(props.a.author!== "" && props.a.author !== null)
+    {
+        authorText = "by "+props.a.author;
+    }
      
     
 
 
     return(
         
-        <div onClick= {() => setShowArticle(!showArticle)} id = "articleTile">
+        <div onClick= {() => setShowArticle(!showArticle)} id = "articleTile" style = {{paddingLeft: "10px"}}>
             <h2 id = "title">{props.a.title}</h2>
-            <h3>by {props.a.author}</h3>
+            <h3>{authorText}</h3>
             <p>{props.a.description}</p>
             <iframe src = {props.a.url} title = {props.a.title} width = "100%" height = "510px" style = {frameStyle}></iframe>
         </div>
