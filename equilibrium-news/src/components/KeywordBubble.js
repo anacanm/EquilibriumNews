e835c31db4cd9e006689b91f273ce2579c5e800c
Jason Douglas <jasondouglas@Jasons-MacBook-Pro.local>
2020-02-27 18:20:50 -0800

    adding Keyword Bubble Styles

--- equilibrium-news/src/components/KeywordBubble.js
+++ equilibrium-news/src/components/KeywordBubble.js
@@ -4,11 +4,19 @@ export const KeywordBubble = (props) =>{
 
     //below is the basic button styling I made to be able to see the buttons, this won't exist (here at least) when you're done -anacan
     const buttonStyle = {
-        borderRadius: "50%",
+        borderRadius: "50px",
         width: "120px",
         height: "75px",
-        backgroundColor: "blue",
-        color: "white"
+        backgroundColor: "black",
+        color: "white",
+        right: "50px",
+        top: "50px",
+        float: "right",
+        // padding: "10px",
+        position: "static",
+        clear: "right",
+        // outlineStyle: "dotted",
+        // outlineColor: "grey"
     }
 
     return(
