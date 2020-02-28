3361f13d465debdfd7617bd42c87e1f3d08c150c
Sumaiyah Lee <sumaiyahlee.sl@gmail.com>
2020-02-27 18:29:57 -0800

    made logo and search bar files for css

--- equilibrium-news/src/components/Logo.js
+++ equilibrium-news/src/components/Logo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../styles/Otto.jpg'
+import "../styles/Logo.css";
 //replace the Otto.jpg in the styles folder and in the line above with the logo once it's made
 
 export const Logo = (props) =>{
--- equilibrium-news/src/components/SearchBar.js
+++ equilibrium-news/src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "../styles/SearchBar.css";
 
 
 export const SearchBar = (props) =>{
