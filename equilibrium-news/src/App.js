8f934746306f721f429c880011b2a6f66bd253a8
anacanm <anacan@mangelsdorf.io>
2020-03-04 15:28:01 -0800

    added routing

--- equilibrium-news/src/App.js
+++ equilibrium-news/src/App.js
@@ -4,9 +4,11 @@ import { KeywordBubbleContainer } from './containers/KeywordBubbleContainer';
 import { ArticlesContainer } from './containers/ArticlesContainer';
 import { AboutUs } from './components/AboutUs';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 export const App = () => {
-    
-    const [query, setQuery] = useState("");  
+
+    const [query, setQuery] = useState("");
 
     //to make more KeywordBubbles just add the keyword you want to this array :)
     const keywords = ["politics", "feminism", "technology", "business", "health"];
@@ -16,12 +18,21 @@ export const App = () => {
     //Lexi, you'll be working with styling the ArticlesContainer and the Article component
     //any styling you choose not to do in the react file (which you can, it's pretty dope), put into the styles folder please
 
-    return(
+    return (
         <div>
-            <TopBarContainer setQuery = {setQuery} query = {query}/>
-            <KeywordBubbleContainer setQuery = {setQuery} keywords = {keywords}/>
-            <ArticlesContainer  query = {query}/>
-            {/* <AboutUs/> */}
+            <Router>
+                <Route exact={true} path="/" render={() =>
+                    <div>
+                        <TopBarContainer setQuery={setQuery} query={query} />
+                        <KeywordBubbleContainer setQuery={setQuery} keywords={keywords} />
+                        <ArticlesContainer query={query} />
+                    </div>
+                } />
+
+                <Route path="/about" render={() => <AboutUs />} />
+
+
+            </Router>
         </div>
     );
 
