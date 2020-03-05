5371bc7cc6c7968aa395cbeac307dc5efdbba192
Lexi Weingardt <aweingar@lion.lmu.edu>
2020-03-04 18:21:42 -0800

    Fixed up article title, author, and description

--- equilibrium-news/src/components/Article.js
+++ equilibrium-news/src/components/Article.js
@@ -4,6 +4,13 @@ import "../styles/Articles.css";
 import Iframe from 'react-iframe'
 
 export const Article = (props) =>{
+
+  const articleStyle = {
+        fontFamily: "Bree Serif",
+        fontWeight: "normal",
+        textAlign: "center",
+        padding: "2% 10% 10% 10%",
+    }
     //I made this component just to see if it would work. It does, but it's all yours to make good. -anacan
     function isAuthor(props) {
       return <h3>by {props.a.author}</h3>;
@@ -32,9 +39,9 @@ export const Article = (props) =>{
     return(
         <div className="iFrame">
         	<div className="innerFrame">
-            <h2>{props.a.title}</h2>
-            <h3>by {props.a.author}</h3>
-            <p styles="font-size: 20px; color:#4a54f1; text-align:center; padding-top:100px;"> {props.a.description}</p>
+            <h2 style={{color: 'black'}}> {props.a.title}</h2>
+            <h3 style={{color: 'darkgray'}}> by {props.a.author}</h3>
+            <p style={{color: 'gray'}}> {props.a.description}</p>
             <Iframe url = {props.a.url} width="450px" height="450px" id="myId" className="myClassname" display="initial" position="relative" onload="iframeclick()" />
             </div>
         </div>
